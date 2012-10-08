bb913f46af0a8c3d960fd277b4982151680c7bef
colinsheppard <colinthegaunt@gmail.com>
2012-10-08 15:09:24 -0700

    some cleanup in taz manipulation, separated aggregation from weighting

---
+++ R/TAZ-aggregate.R
@@ -0,0 +1,169 @@
+######################################################################################################
+# TAZ AGGREGATE
+# 
+# Take the original TAZ data and corresponding travel demand data and aggregated
+# based on polygons generated in google earth (and converted to shape files in QGIS)
+######################################################################################################
+
+library(colinmisc)
+load.libraries(c('maptools','plotrix','stats','gpclib','plyr','png','RgoogleMaps','lattice','stringr','ggplot2','rgdal','XML','plotKML'))
+gpclibPermit()
+
+path.to.geatm  <- '~/Dropbox/serc/pev-colin/data/GEATM-2020/'
+path.to.google <- '~/Dropbox/serc/pev-colin/data/google-earth/'
+path.to.humveh <- '~/Dropbox/serc/pev-colin/data/Vehicle-Registration/'
+path.to.plots  <- '~/Dropbox/serc/pev-colin/plots/'
+path.to.pevi   <- '~/Dropbox/serc/pev-colin/pevi/'
+path.to.parcel <- '~/Dropbox/serc/pev-colin/data/HUM-PARCELS/'
+
+source(paste(path.to.pevi,'R/gis-functions.R',sep=''))
+
+taz <- readShapePoly(paste(path.to.geatm,'Shape_Files/taz-LATLON.shp',sep=''))
+
+# load the OD data
+od.24.old <- read.table(paste(path.to.geatm,'OD_Tables/OD by type 24 hr (2020).txt',sep=''),header=FALSE, sep=",",colClasses='numeric')
+names(od.24.old) <- c('from','to','hbw','hbshop','hbelem','hbuniv','hbro','nhb','ix','xi','ee','demand')
+od.am.old <- read.table(paste(path.to.geatm,'OD_Tables/OD by type AM (2020).txt',sep=''),header=FALSE, sep=",",colClasses='numeric')
+names(od.am.old) <- c('from','to','hbw','hbshop','hbelem','hbuniv','hbro','nhb','ix','xi','ee','demand')
+od.pm.old <- read.table(paste(path.to.geatm,'OD_Tables/OD by type PM (2020).txt',sep=''),header=FALSE, sep=",",colClasses='numeric')
+names(od.pm.old) <- c('from','to','hbw','hbshop','hbelem','hbuniv','hbro','nhb','ix','xi','ee','demand')
+
+# Sum demand to get a total
+od.24.sum <- ddply(od.24.old,.(from),function(df){ sum(df$demand) })
+names(od.24.sum) <- c('taz','demand')
+taz@data$total.demand <- od.24.sum$demand[match(taz@data$NEWTAZ,od.24.sum$taz)]
+taz@data$total.demand.per.acre <- taz@data$total.demand / taz@data$ACRES
+
+# Make a plot of total demand disaggregated
+#trellis.par.set(sp.theme())
+#spplot(taz,'total.demand')
+#spplot(taz,'total.demand.per.acre')
+
+# Load the aggregation polygons
+agg.polys <- readShapePoly(paste(path.to.google,'ProposedAggregation.shp',sep=''))
+taz.centroids <-SpatialPointsDataFrame(coordinates(taz),data=data.frame(longitude= coordinates(taz)[,1],latitude= coordinates(taz)[,2]))
+agg.mapping <- data.frame(name=over(taz.centroids,agg.polys)$Name)
+agg.mapping$agg.id <- as.numeric(agg.mapping$name)
+agg.taz.shp <- unionSpatialPolygons(taz,agg.mapping$agg.id)
+aggregate.data <- function(df){ 
+ return( colSums(df[,c('AREA','ACRES','SHAPE_AREA')]) ) 
+}
+taz@data$agg.id <- agg.mapping$agg.id
+agg.taz.data <- ddply(taz@data,.(agg.id),aggregate.data)
+agg.taz.shp <- SpatialPolygonsDataFrame(agg.taz.shp,agg.taz.data)
+
+# add new zone numbers corresponding to old zone numbers to the dataframe
+od.24.old$from.new <- taz$agg.id[match(od.24.old$from,taz$NEWTAZ)]
+od.24.old$to.new <- taz$agg.id[match(od.24.old$to,taz$NEWTAZ)]
+od.am.old$from.new <- taz$agg.id[match(od.am.old$from,taz$NEWTAZ)]
+od.am.old$to.new <- taz$agg.id[match(od.am.old$to,taz$NEWTAZ)]
+od.pm.old$from.new <- taz$agg.id[match(od.pm.old$from,taz$NEWTAZ)]
+od.pm.old$to.new <- taz$agg.id[match(od.pm.old$to,taz$NEWTAZ)]
+
+# for now, omit the rows with NA, which correspond to the TAZ id's in the OD data which don't have a
+# corresponding entry in the TAZ shape file (ID's 11-20)
+# I think this also omits ID's 1-10
+od.24.new <- ddply(na.omit(od.24.old),.(from.new,to.new),function(df){ 
+  c(sum(df$hbw),sum(df$hbshop),sum(df$hbelem),sum(df$hbuniv),sum(df$hbro),sum(df$nhb),sum(df$ix),sum(df$xi),sum(df$ee),sum(df$demand)) })
+names(od.24.new) <- c('from','to','hbw','hbshop','hbelem','hbuniv','hbro','nhb','ix','xi','ee','demand')
+od.am.new <- ddply(na.omit(od.am.old),.(from.new,to.new),function(df){
+  c(sum(df$hbw),sum(df$hbshop),sum(df$hbelem),sum(df$hbuniv),sum(df$hbro),sum(df$nhb),sum(df$ix),sum(df$xi),sum(df$ee),sum(df$demand)) })
+names(od.am.new) <- c('from','to','hbw','hbshop','hbelem','hbuniv','hbro','nhb','ix','xi','ee','demand')
+od.pm.new <- ddply(na.omit(od.pm.old),.(from.new,to.new),function(df){ 
+  c(sum(df$hbw),sum(df$hbshop),sum(df$hbelem),sum(df$hbuniv),sum(df$hbro),sum(df$nhb),sum(df$ix),sum(df$xi),sum(df$ee),sum(df$demand)) })
+names(od.pm.new) <- c('from','to','hbw','hbshop','hbelem','hbuniv','hbro','nhb','ix','xi','ee','demand')
+
+write.csv(od.24.new,file=paste(path.to.geatm,'od_24_new.csv',sep=''))
+write.csv(od.am.new,file=paste(path.to.geatm,'od_am_new.csv',sep=''))
+write.csv(od.pm.new,file=paste(path.to.geatm,'od_pm_new.csv',sep=''))
+
+# check that the sum of the rows equals the value in the sum column
+# they are currently not quite equal and I suspect this is from omiting zones 1-10
+od.24.new$row.sum <- rowSums(od.24.new[,3:11])
+
+# store total traffic leaving each new zone
+od.24.sum.from <- ddply(od.24.new,.(from),function(df){ sum(df$demand) })
+names(od.24.sum.from) <- c('taz','demand')
+od.24.sum.to <- ddply(od.24.new,.(to),function(df){ sum(df$demand) })
+names(od.24.sum.to) <- c('taz','demand')
+
+if(names(agg.taz.shp@data)[1]!="id")names(agg.taz.shp@data) <- c('id',names(agg.taz.shp@data)[2:ncol(agg.taz.shp@data)]) # make first column of shape data 'id' instead of 'agg.id'
+
+# add total demand to the existing shape data
+agg.taz.shp@data$total.demand.from <- od.24.sum.from$demand[agg.taz.shp$id]
+agg.taz.shp@data$total.demand.to <- od.24.sum.to$demand[agg.taz.shp$id]
+
+trellis.par.set(sp.theme())
+spplot(agg.taz.shp,c('total.demand.from','total.demand.to'))
+
+# look at where people from each TAZ are going to 
+agg.names <- as.character(ddply(agg.mapping,.(agg.id),function(df){ df[1,]})$name)
+agg.taz.shp@data$name <- agg.names[agg.taz.shp@data$id]
+
+#agg.points <- fortify(agg.taz.shp,region="id")
+#agg.df <- join(agg.points,agg.taz.shp@data,by="id")
+
+#ggplot(agg.df,aes(long,lat,group=name))+
+  #geom_polygon(aes(fill=total.demand)) +
+  #geom_path(color="white") +
+  #coord_equal()
+
+#make.dir(paste(path.to.plots,'demand',sep=''))
+#for(agg.taz.id in agg.taz.shp$id){
+  #taz.name <- str_replace_all(as.character(agg.mapping$name[agg.mapping$agg.id == agg.taz.id][1]),'-','.')
+  #if(!is.na(as.numeric(substr(taz.name,1,1)))) taz.name <- paste('HWY',taz.name,sep='')
+  #new.col.name.to <- paste(taz.name,'.to',sep='')
+  #new.col.name.from <- paste(taz.name,'.of.total',sep='')
+  
+  #agg.taz.shp@data[,new.col.name.to] <- subset(od.24.new,from==agg.taz.id)$demand[agg.taz.shp$id]/sum(subset(od.24.new,from==agg.taz.id)$demand)*100
+  #agg.taz.shp@data[,new.col.name.from] <- subset(od.24.new,to==agg.taz.id)$demand[agg.taz.shp$id]/sum(od.24.new$demand)*100
+  #pdf(file=paste(path.to.plots,'demand/',taz.name,'.pdf',sep=''),16,12)
+  #trellis.par.set(sp.theme())
+  #print(spplot(agg.taz.shp,c(new.col.name.to,new.col.name.from)))
+  #dev.off()
+#}
+#spplot(agg.taz.shp,names(agg.taz.shp@data)[grep('of.total',names(agg.taz.shp@data))], names.attr= agg.taz.shp$name,colorkey=list(space="bottom"))
+
+# write the data to a shapefile, note we need to write the names of the fields separately b/c they get truncated due to ESRI format limitations
+writePolyShape(agg.taz.shp,paste(path.to.pevi,'inputs/development/aggregated-taz',sep=''))
+agg.taz.shp.fieldnames <- names(agg.taz.shp@data)
+save(agg.taz.shp.fieldnames,file=paste(path.to.pevi,'inputs/development/aggregated-taz-fieldnames.Rdata',sep=''))
+
+
+# EXTRA PLOTTING IN G-EARTH
+
+# write the data to KML file with colors related to traffic demand
+c.map <- paste(map.color(agg.taz.shp@data$total.demand.from,blue2red(50)),'7F',sep='')
+shp.to.kml(agg.taz.shp,paste(path.to.pevi,'inputs/development/aggregated-taz.kml',sep=''),'Aggregated TAZs','Color denotes total daily demand','red',1.5,c.map,name.col='name',description.cols=names(agg.taz.shp@data))
+
+c.map <- paste(map.color(taz@data$total.demand,blue2red(50)),'7F',sep='')
+ shp.to.kml(taz,paste(path.to.pevi,'inputs/development/disaggregated-taz.kml',sep=''),'Disaggregated TAZs','Color denotes total daily demand','white',1.5,c.map,name.col='NEWTAZ',description.cols=c('total.demand','NEWTAZ','ACRES'),id.col='ID')
+
+for.c.map <- log(taz@data$total.demand.per.acre[-which(taz@data$total.demand.per.acre==Inf)])
+for.c.map[for.c.map<=0] <- 0 
+c.map <- paste(map.color(for.c.map,blue2red(50)),'7F',sep='')
+ shp.to.kml(taz[-which(taz@data$total.demand.per.acre==Inf),],paste(path.to.pevi,'inputs/development/disaggregated-taz-per-acre.kml',sep=''),'Disaggregated TAZs','Color denotes total daily demand per acre','white',1.5,c.map,name.col='NEWTAZ',description.cols=c('total.demand.per.acre','total.demand','NEWTAZ','ACRES'),id.col='ID')
+
+# Join Humboldt parcel data with land use data (note: experimented with this but nevery actually used parcel data for anything)
+
+#land.use <- read.dbf(paste(path.to.parcel,'Hum-Land-Use.dbf',sep=''))$dbf
+#subset(land.use,APN==50303106)
+#apn <- readShapePoly(paste(path.to.parcel,'Hum-Parcels-LongLat.shp',sep=''))
+#apn@data <- join(apn@data,land.use[,c('APN','USECODE','NEIGHCODE',"SITHSNBR","SITHSNBRSF","SITSTDIR","SITSTNAME", "SITSTTYPE","SITCITY","SITZIP","ZONING","GEN_PLAN")],by="APN")
+#use.codes <- read.csv(paste(path.to.parcel,'usecodes.csv',sep=''),colClasses=c('character','character','character','numeric'))
+#apn@data <- join(apn@data,use.codes,by="NEIGHCODE")
+#apn@data$color <- ifelse( apn@data$type == 'residential','#E300007F',ifelse(apn@data$type=='commercial','#004FE37F',ifelse(apn@data$type=='industrial','#00C4107F','#0000007F')))
+#apn@data$id <- sapply(slot(apn,'polygons'),function(x){ as.numeric(slot(x,'ID')) })
+#apn@data$use<-as.character(apn@data$use)
+#apn@data$type<-as.character(apn@data$type)
+#apn@data$EXLU4<-as.character(apn@data$EXLU4)
+#apn@data$ZONING<-as.character(apn@data$ZONING)
+
+#apn.cent <- coordinates(apn)
+#apn.sub <- apn[apn.cent[,1]>-124.14117 & apn.cent[,1] < -124.086455 & apn.cent[,2] < 40.874601 & apn.cent[,2] > 40.799047,]
+
+#shp.to.kml(apn.sub,paste(path.to.parcel,'apn-subset.kml',sep=''),'Humboldt Parcels','Color denotes use category','white',0.5,apn.sub@data$color,name.col='APN',description.cols=c('EXLU4','ZONING','use','type','weight'))
+
+#shp.to.kml(apn,paste(path.to.parcel,'apn.kml',sep=''),'Humboldt Parcels','Color denotes use category','white',0.5,apn@data$color,name.col='APN',description.cols=c('EXLU4','ZONING','use','type','weight'))
+
+
---
+++ R/TAZ-weighting.R
@@ -0,0 +1,173 @@
+library(colinmisc)
+load.libraries(c('maptools','plotrix','stats','gpclib','plyr','png','RgoogleMaps','lattice','stringr','ggplot2','rgdal','XML','plotKML'))
+gpclibPermit()
+
+path.to.geatm  <- '~/Dropbox/serc/pev-colin/data/GEATM-2020/'
+path.to.google <- '~/Dropbox/serc/pev-colin/data/google-earth/'
+path.to.humveh <- '~/Dropbox/serc/pev-colin/data/Vehicle-Registration/'
+path.to.plots  <- '~/Dropbox/serc/pev-colin/plots/'
+path.to.pevi   <- '~/Dropbox/serc/pev-colin/pevi/'
+path.to.parcel <- '~/Dropbox/serc/pev-colin/data/HUM-PARCELS/'
+
+source(paste(path.to.pevi,'R/gis-functions.R',sep=''))
+
+zips    <- readShapePoly(paste(path.to.geatm,'../CA-ZIPS/tl_2010_06_zcta510.shp',sep=''))
+zips@data$INTPTLAT10 <- as.numeric(as.character(zips@data$INTPTLAT10))
+zips@data$INTPTLON10 <- as.numeric(as.character(zips@data$INTPTLON10))
+hum.zips<- which(zips@data$INTPTLAT10 < 42 & zips@data$INTPTLAT10 > 39.5 & zips@data$INTPTLON10 < -123 & zips@data$INTPTLON10 > -124.5 )
+
+tracts <- readShapePoly(paste(path.to.geatm,'../HUM-CENSUS-TRACTS/hum-census-tracts.shp',sep=''))
+tracts <- tracts[-which(tracts@data$NAME10=='9901'),]
+tracts@data$GEOID10 <- as.numeric(as.character(tracts@data$GEOID10))
+tracts@data$NAMELSAD10 <- as.character(tracts@data$NAMELSAD10)
+tract.pop <- read.csv(paste(path.to.geatm,'../HUM-CENSUS-TRACTS/2010-population-by-tract.csv',sep=''))
+tracts@data$population <- tract.pop$total.population[match(tracts@data$GEOID10,tract.pop$GEO.id)]
+tracts@data$ID <- unlist(lapply(tracts@polygons,function(x){slot(x,'ID')}))
+#c.map <- paste(map.color(tracts@data$population,blue2red(50)),'7F',sep='')
+#shp.to.kml(tracts,paste(path.to.google,'humboldt-census-tracts.kml',sep=''),'Population','','red',1.5,c.map,id.col='ID',name.col='name',description.cols=c('NAMELSAD10','population'))
+
+
+# finding two overlapping polygons
+#taz.id <- which(taz@data$ID==519)
+#zip.id <- which(zips@data$ZCTA5CE10==95549)
+#plot(taz[taz.id,],col='#ee229922',add=T)
+#plot(zips[zip.id,],col='#3399ff22',add=T)
+
+
+if(!file.exists(paste(path.to.geatm,"zip-and-tract-fraction-in-taz-matrix.Rdata",sep=''))){
+  # get the data frame ready for storage of the results which will hold the fraction of each zip (the columns) by area that 
+  # are in each TAZ (the rows), where the 'null' column contains the fraction of the TAZ with no associated zip code
+  zips.in.taz <- as.data.frame(matrix(0,nrow(taz@data),length(hum.zips)+1))
+  names(zips.in.taz) <- c('null',as.character(zips[hum.zips,]@data$ZCTA5CE10))
+  row.names(zips.in.taz) <- as.character(sort(taz@data$NEWTAZ))
+
+  tracts.in.taz <- as.data.frame(matrix(0,nrow(taz@data),nrow(tracts@data)))
+  names(tracts.in.taz) <- as.character(tracts@data$NAME10)
+  row.names(tracts.in.taz) <- as.character(sort(taz@data$NEWTAZ))
+
+  # precalculate the area of each zip/tract polygons to reduce redundancy
+  zip.areas <- list()
+  for(zip.i in names(zips.in.taz)[2:ncol(zips.in.taz)]){
+    zip.id <- which(zips@data$ZCTA5CE10 == zip.i)
+    zip.areas[[zip.i]] <- area.of.union(zips[zip.id,])
+  }
+  tract.areas <- list()
+  for(tract.i in names(tracts.in.taz)){
+    tract.id <- which(tracts@data$NAME10 == tract.i)
+    tract.areas[[tract.i]] <- area.of.union(tracts[tract.id,])
+  }
+
+  for(taz.i in row.names(zips.in.taz)){
+    print(taz.i)
+    taz.id <- which(taz@data$NEWTAZ == taz.i)
+    taz.area <- area.of.union(taz[taz.id,])
+    cum.frac <- 0
+    for(zip.i in names(zips.in.taz)[2:ncol(zips.in.taz)]){
+      if(cum.frac >= 1)next
+      zip.id <- which(zips@data$ZCTA5CE10 == zip.i)
+      # now calculate the fraction by area of zip.i in taz.i
+      zips.in.taz[taz.i,zip.i] <- (taz.area + zip.areas[[zip.i]] - area.of.union(list(taz[taz.id,],zips[zip.id,]))) / taz.area
+      cum.frac <- cum.frac + zips.in.taz[taz.i,zip.i]
+    }
+    cum.frac <- 0
+    for(tract.i in names(tracts.in.taz)[1:ncol(tracts.in.taz)]){
+      if(cum.frac >= 1)next
+      tract.id <- which(tracts@data$NAME10 == tract.i)
+      # now calculate the fraction by area of tract.i in taz.i
+      tracts.in.taz[taz.i,tract.i] <- (taz.area + tract.areas[[tract.i]] - area.of.union(list(taz[taz.id,],tracts[tract.id,]))) / taz.area
+      cum.frac <- cum.frac + tracts.in.taz[taz.i,tract.i]
+    }
+  }
+  zips.in.taz[,'null'] <- apply(zips.in.taz[,2:ncol(zips.in.taz)],1,function(x){ 1-sum(x) }) 
+  # the first 10 rows are the boundary TAZs which should have 1 for null and zero for all other zips and for all tracts
+  zips.in.taz[1:10,'null'] <- 1
+  zips.in.taz[1:10,2:ncol(zips.in.taz)] <- 0
+  tracts.in.taz[1:10,] <- 0
+  # remove 0 columns which represent zip codes or tracts outside of humboldt
+  zips.in.taz <- zips.in.taz[,-which(apply(zips.in.taz,2,sum)<=0)]
+  tracts.in.taz <- tracts.in.taz[,-which(apply(tracts.in.taz,2,sum)<=0)]
+  save(zips.in.taz,tracts.in.taz,file=paste(path.to.geatm,"zip-and-tract-fraction-in-taz-matrix.Rdata",sep=''))
+}else{
+  load(paste(path.to.geatm,"zip-and-tract-fraction-in-taz-matrix.Rdata",sep=''))
+}
+
+# now load up the data providing the fraction of EV's and Hybrids in Humboldt by zipcode and year from 2003-2011
+load(paste(path.to.humveh,'veh.Rdata',sep=''))  # veh
+load(file=paste(path.to.humveh,'tot-frac-by-year.Rdata',sep='')) # tot.by.year, frac.by.year
+aggregate.fracs <- ddply(veh,.(FUEL.TYPE,year),function(df){ data.frame(frac=sum(df$COUNT,na.rm=T)/subset(tot.by.year,year==df$year[1])$count) })
+frac.weight.by.zip.year <- ddply(frac.by.zip.year,.(FUEL.TYPE,zip.city,year),function(df){ data.frame(frac.weight=df$frac/subset(aggregate.fracs,year==df$year[1] & FUEL.TYPE==df$FUEL.TYPE[1])$frac) })
+
+# plot those weights
+ggplot(subset(frac.weight.by.zip.year,FUEL.TYPE%in%c("GAS/ELEC","ELECTRIC")),aes(x=year,y=frac.weight))+geom_bar(stat="identity",position="dodge",aes(fill=FUEL.TYPE))+facet_wrap(~zip.city)+scale_y_continuous(name="Ratio of Zip-Level Penetration to Aggregated Penetration")
+
+# do it again but aggregate EV/Hybrids first 
+frac.weight.by.zip.year.simple <- ddply(subset(frac.by.zip.year,FUEL.TYPE%in%c("GAS/ELEC","ELECTRIC")),.(zip.city,year),function(df){ data.frame(zip=df$zip[1],frac.weight=sum(df$frac)/sum(subset(aggregate.fracs,year==df$year[1] & FUEL.TYPE %in% df$FUEL.TYPE)$frac)) })
+ggplot(frac.weight.by.zip.year.simple,aes(x=year,y=frac.weight))+geom_bar(stat="identity")+facet_wrap(~zip.city)+scale_y_continuous(name="Ratio of Zip-Level Penetration to Aggregate Penetration")
+
+# Finally, take the average ratio in each zip code over the 7 year time frame
+frac.est <- ddply(frac.weight.by.zip.year.simple,.(zip),function(df){ data.frame(frac.weight=mean(df$frac.weight)) })
+
+# now dump results from PO Box only zip codes 95502 95518 95534 into their surrounding zips that actually have geographic extension
+frac.est$frac.weight[frac.est$zip==95521] <- sum(frac.est$frac.weight[frac.est$zip%in%c(95521,95518)])
+frac.est$frac.weight[frac.est$zip==95503] <- sum(frac.est$frac.weight[frac.est$zip%in%c(95503,95534)])
+frac.est$frac.weight[frac.est$zip==95501] <- sum(frac.est$frac.weight[frac.est$zip%in%c(95501,95502)])
+frac.est <- frac.est[!frac.est$zip %in% c(95502,95518,95534),]
+
+# for zip codes that weren't in the polk data (mostly border zips outside of humboldt with tiny fractions inside TAZs), we add rows to frac.est make them have a weight of 1
+frac.est <- rbind(frac.est,data.frame(zip=names(zips.in.taz)[! names(zips.in.taz) %in% frac.est$zip],frac.weight=1))
+
+# create the distance matrix on the original TAZ data (@ 40 deg N it's ~53 miles / deg long and ~69 miles / deg lat)
+coords.in.miles <- data.frame(x=coordinates(taz)[,1]*53,y=coordinates(taz)[,2]*69)
+d <- as.matrix(dist(coords.in.miles))
+# create corresponding matrix of weights on each row of distance matrix where a quadratic relationship favoring proximity is established
+w.by.distance <- matrix(0,nrow(d),ncol(d))
+max.d <- max(d)
+for(row.i in 1:nrow(d)){
+  w.by.distance[row.i,row.i] <- 0.5
+  top.10 <- sort(d[row.i,-row.i])[1:round(ncol(d)/10)]
+  top.10.inds <- as.numeric(names(top.10))
+  w.by.distance[row.i,top.10.inds] <- (max.d-top.10)^10/sum((max.d-top.10)^10)/2
+}
+
+# now apply these estimates to the matrix
+w.by.penetration <- frac.est$frac.weight[match(names(zips.in.taz),frac.est$zip)]
+w.by.penetration.matrix <- t(apply(zips.in.taz,1,function(x){ x * w.by.penetration }))
+taz.weights.by.penetration <- apply(w.by.penetration.matrix,1,sum)
+
+# for testing
+to.keep <- 100:120
+od <- od.24.old[od.24.old$from %in% to.keep & od.24.old$to %in% to.keep,]
+od <- od.24.old
+od.weighted <- od
+for(taz.i in 1:length(taz.weights.by.penetration)){
+  newtaz <- as.numeric(names(taz.weights.by.penetration[taz.i]))
+  cat(newtaz)
+  od.from.inds  <- which(od$from==newtaz)
+  od.to.inds    <- which(od$to  ==newtaz)
+  od.from.diff  <- (taz.weights.by.penetration[taz.i] - 1) * sum(od$demand[od.from.inds])
+  od.to.diff  <- (taz.weights.by.penetration[taz.i] - 1) * sum(od$demand[od.to.inds])
+
+  # now distribute those diffs based on the weights.by.distance matrix
+  for(distance.weight.i in which(w.by.distance[taz.i,] > 0)){
+    distance.weight     <- w.by.distance[taz.i,distance.weight.i]
+    distance.weight.taz <- as.numeric(names(taz.weights.by.penetration[distance.weight.i]))
+    #from
+    from.to.distribute  <- od.from.diff * distance.weight
+    distance.weight.from.inds <- which(od$from==distance.weight.taz)
+    if(length(distance.weight.from.inds)>0){
+      from.distributed.additive <- from.to.distribute * od.weighted$demand[distance.weight.from.inds] / sum(od.weighted$demand[distance.weight.from.inds])
+      from.distributed.multiplicative <- (from.distributed.additive + od.weighted$demand[distance.weight.from.inds])/ od.weighted$demand[distance.weight.from.inds]
+      from.distributed.multiplicative[is.nan(from.distributed.multiplicative)] <- 1
+      od.weighted[distance.weight.from.inds,ncol(od)] <- od.weighted[distance.weight.from.inds,ncol(od)] * from.distributed.multiplicative
+    }
+    #to
+    to.to.distribute    <- od.to.diff * distance.weight
+    distance.weight.to.inds   <- which(od$to==distance.weight.taz)
+    if(length(distance.weight.to.inds)>0){
+      to.distributed.additive <- to.to.distribute * od.weighted$demand[distance.weight.to.inds] / sum(od.weighted$demand[distance.weight.to.inds])
+      to.distributed.multiplicative <- (to.distributed.additive + od.weighted$demand[distance.weight.to.inds])/od.weighted$demand[distance.weight.to.inds]
+      to.distributed.multiplicative[is.nan(to.distributed.multiplicative)] <- 1
+      od.weighted[distance.weight.to.inds,ncol(od)] <- od.weighted[distance.weight.to.inds,ncol(od)] * to.distributed.multiplicative
+    }
+  }
+}

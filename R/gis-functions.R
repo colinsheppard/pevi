90d5a3c0e9d9865d2da2f186424e94819c355791
Colin Sheppard <colinthegaunt@gmail.com>
2013-01-28 22:08:40 -0800

    new shp.to.kml file which will be used to produce kml with placemarks

--- R/gis-functions.R
+++ R/gis-functions.R
@@ -55,6 +55,32 @@ shp.to.kml <- function(shp,kml.filename,kmlname="KML Name", kmldescription="<i>D
   kmlFile <- file(kml.filename, "w")
   cat(kmlPolygon(kmlname=kmlname, kmldescription=kmldescription)$header, 
       file=kmlFile, sep="\n")
+  ## ADD code to write placemarks
+  cat(unlist(kml.data["style",]), file=kmlFile, sep="\n")
+  cat(unlist(kml.data["content",]), file=kmlFile, sep="\n")
+  cat(kmlPolygon()$footer, file=kmlFile, sep="\n")
+  close(kmlFile)
+
+  system(paste('open ',kml.filename,sep=''))
+}
+
+shp.to.kml <- function(shp,kml.filename,kmlname="KML Name", kmldescription="<i>Description</i>",borders='white',lwds=1.5,colors='red',id.col='id',name.col='id',description.cols=NA){
+  n <- length(shp@polygons)
+  if(length(colors)==1)colors <- rep(colors,n)
+  if(length(borders)==1)borders <- rep(borders,n)
+  if(length(lwds)==1)lwds <- rep(lwds,n)
+  kml.data <- sapply(slot(shp, "polygons"), function(x) { 
+    row.num = which(as.numeric(slot(x, "ID"))==shp@data[[id.col]])
+    descrip = ifelse(is.na(description.cols),'',paste(paste(description.cols,': ',shp@data[row.num,description.cols],sep=''),collapse='<br/><br/>')) 
+    kmlPolygon(x,
+      name=shp@data[[name.col]][row.num], 
+      col=colors[row.num], lwd=lwds[row.num], border=borders[row.num], 
+      description=descrip
+    )})
+
+  kmlFile <- file(kml.filename, "w")
+  cat(kmlPolygon(kmlname=kmlname, kmldescription=kmldescription)$header, 
+      file=kmlFile, sep="\n")
   cat(unlist(kml.data["style",]), file=kmlFile, sep="\n")
   cat(unlist(kml.data["content",]), file=kmlFile, sep="\n")
   cat(kmlPolygon()$footer, file=kmlFile, sep="\n")

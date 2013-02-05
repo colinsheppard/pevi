86dfb881c691c931b690477cc8caf75d4de1f6b1
Colin Sheppard <colinthegaunt@gmail.com>
2013-02-05 08:16:05 -0800

    added 'linked' capability to buildout which transitions to new penetration at the conclusion of previous

--- R/optim/analyze-buildout.R
+++ R/optim/analyze-buildout.R
@@ -6,7 +6,10 @@ base.path <- '/Users/critter/Dropbox/serc/pev-colin/'
 #base.path <- '/Users/sheppardc/Dropbox/serc/pev-colin/'
 #base.path <- '/Users/Raskolnikovbot3001/Dropbox/'
 
-optim.code <- 'min-cost-constrained-by-frac-stranded'
+link.pens <- F 
+
+#optim.code <- 'linked-min-cost-constrained-by-frac-stranded-50-50'
+optim.code <- 'min-cost-constrained-by-frac-stranded-50-50'
 optim.code.date <- paste(optim.code,"-",format(Sys.time(), "%Y%m%d"),sep='')
 
 path.to.pevi <- paste(base.path,'pevi/',sep='')
@@ -29,19 +32,24 @@ load(paste(path.to.google,'aggregated-taz-with-weights/aggregated-taz-with-weigh
 names(agg.taz@data) <- c('row',taz.shp.fieldnames)
 agg.taz@data$ID <- unlist(lapply(agg.taz@polygons,function(x){slot(x,'ID')}))
 
-optim.code <- 'min-cost-constrained-by-frac-stranded'
 
 #pev.penetration <- 0.005
 for(pev.penetration in c(0.005,0.01,0.02,0.04)){
   build.files <- data.frame(file=list.files(path.to.outputs,paste('^buildout-pen',pev.penetration*100,'.*csv',sep='')),stringsAsFactors=F)
   build.files$iter <- as.numeric(sapply(strsplit(build.files$file,'-iter',fixed=T),function(x){ strsplit(x[2],'-',fixed=T)[[1]][1] }))
-  build.files <- build.files[match(1:nrow(build.files),build.files$iter),]
+  build.files <- build.files[order(build.files$iter),]
   n.iter <- nrow(build.files)
 
-  build.res <- data.frame(iter=rep(build.files$iter,each=105),taz=rep(c(1:52,1:52,0),nrow(build.files)),level=c(rep(2,52),rep(3,52),0),cost=NA,pain=NA,chargers=NA,marg.cost.of.abatement=NA)
+  if(!link.pens | pev.penetration==0.005){
+    build.res <- data.frame(iter=rep(build.files$iter,each=105),taz=rep(c(1:52,1:52,0),nrow(build.files)),level=c(rep(2,52),rep(3,52),0),cost=NA,pain=NA,chargers=NA,marg.cost.of.abatement=NA,pen=pev.penetration)
+    all.build.rows <- 1:nrow(build.res)
+  }else{
+    build.res <- rbind(build.res,data.frame(iter=rep(build.files$iter,each=105),taz=rep(c(1:52,1:52,0),nrow(build.files)),level=c(rep(2,52),rep(3,52),0),cost=NA,pain=NA,chargers=NA,marg.cost.of.abatement=NA,first=NA,name=NA,pen=pev.penetration))
+    all.build.rows <- which(build.res$iter %in% build.files$iter)
+  }
   
   for(build.i in 1:n.iter){
-    build.res[build.res$iter==build.i,c('cost','pain','chargers','marg.cost.of.abatement')] <- read.csv(paste(path.to.outputs,build.files$file[build.i],sep=''))[,c('cost','pain','chargers','marg.cost.of.abatement')]
+    build.res[build.res$iter==build.files$iter[build.i],c('cost','pain','chargers','marg.cost.of.abatement')] <- read.csv(paste(path.to.outputs,build.files$file[build.i],sep=''))[,c('cost','pain','chargers','marg.cost.of.abatement')]
   }
 
   build.res$name <- agg.taz$name[match(build.res$taz,agg.taz$id)]
@@ -53,9 +61,19 @@ for(pev.penetration in c(0.005,0.01,0.02,0.04)){
   build.res$first <- first$first[match(build.res$taz,first$taz)]
   build.res$name <- reorder(build.res$name,build.res$first)
 
-  # facet by name
-  p <- ggplot(subset(build.res,level>0),aes(x=iter,y=chargers,fill=factor(level),width=1))+geom_bar(stat='identity')+facet_wrap(~name)+opts(title=paste("Loading Order, ",pev.penetration*100,"% Penetration",sep='')) + labs(x="Nth Charger Added", y="Cumulative Number of Chargers in Zone",fill="Charger Level") 
-  ggsave(paste(path.to.outputs,"plots/",optim.code.date,"/loading-order-pen",pev.penetration*100,".pdf",sep=''),p,width=15,height=11)
-
+  if(!link.pens){
+    # facet by name
+    p <- ggplot(subset(build.res,level>0),aes(x=iter,y=chargers,fill=factor(level),width=1))+geom_bar(stat='identity')+facet_wrap(~name)+opts(title=paste("Loading Order, ",pev.penetration*100,"% Penetration",sep='')) + labs(x="Nth Charger Added", y="Cumulative Number of Chargers in Zone",fill="Charger Level") 
+    ggsave(paste(path.to.outputs,"plots/",optim.code.date,"/loading-order-pen",pev.penetration*100,".pdf",sep=''),p,width=15,height=11)
+  }
 }
 
+build.res$name <- agg.taz$name[match(build.res$taz,agg.taz$id)]
+first <- ddply(build.res,.(taz),function(df){ data.frame(first=ifelse(sum(df$chargers)>0,subset(df,chargers>0)$iter[1],9999)) })
+build.res$first <- first$first[match(build.res$taz,first$taz)]
+build.res$name <- reorder(build.res$name,build.res$first)
+
+
+pen.transitions <- ddply(build.res,.(pen),function(df){ df$iter[1] })
+p <- ggplot(subset(build.res,level>0),aes(x=iter,y=chargers,fill=factor(level),width=1))+geom_bar(stat='identity')+geom_vline(data=pen.transitions,aes(xintercept=V1),colour='grey')+facet_wrap(~name)+opts(title=paste("Loading Order, Linked Penetrations",sep='')) + labs(x="Nth Charger Added", y="Cumulative Number of Chargers in Zone",fill="Charger Level") 
+ggsave(paste(path.to.outputs,"plots/",optim.code.date,"/loading-order-linked-pens.pdf",sep=''),p,width=15,height=11)

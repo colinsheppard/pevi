14dc5c07f0f8c69e126eef6f5218e163569d5a01
Andrew Harris <Raskolnikovbot3001@Andrews-MacBook-Pro.local>
2013-02-11 17:11:31 -0800

    Added code to create kml of electric vehicle demand

--- R/gis-functions.R
+++ R/gis-functions.R
@@ -234,4 +234,151 @@ shp.to.kml <- function(shp,kml.filename,kmlname="KML Name", kmldescription="<i>D
 
   system(paste('open ',kml.filename,sep=''))
 }
+
+demand.to.kml <- function(shp,kml.filename,kmlname="KML Name", kmldescription="<i>Description</i>",borders='white',lwds=1.5,colors='red',id.col='id',name.col='id',description.cols=NA){
+    
+  n <- length(shp@polygons)
+  if(length(colors)==1)colors <- rep(colors,n)
+  if(length(borders)==1)borders <- rep(borders,n)
+  if(length(lwds)==1)lwds <- rep(lwds,n)
+  kml.data <- sapply(slot(shp, "polygons"), function(x) { 
+    row.num = which(as.numeric(slot(x, "ID"))==shp@data[[id.col]])
+    descrip = ifelse(is.na(description.cols),'',paste(paste(description.cols,': ',shp@data[row.num,description.cols],sep=''),collapse='<br/><br/>')) 
+    kmlPolygon(x,
+      name=shp@data[[name.col]][row.num], 
+      col=colors[row.num], lwd=lwds[row.num], border=borders[row.num], 
+      description=descrip
+    )})
+
+  kmlFile <- file(kml.filename, "w")
+  cat(kmlPolygon(kmlname=kmlname, kmldescription=kmldescription)$header, 
+      file=kmlFile, sep="\n")
+  shp$long <- coordinates(shp)[,1]
+  shp$lat  <- coordinates(shp)[,2]
+  shp$newname <- NA
+  eka.dense.rows <- c(grep("EKA_",shp$name),which(shp$name %in% c("Bayview","Cutten","Myrtletown","FieldsLanding")))
+  mck.dense.rows <- grep("MCK_",shp$name)
+  for.dense.rows <- grep("FOR_",shp$name)
+  arc.dense.rows <- grep("ARC_",shp$name)
+  dense.rows <- c(eka.dense.rows,mck.dense.rows,for.dense.rows,arc.dense.rows)
+  shp.sparse <- rbind(collapse.df(shp@data[eka.dense.rows,],"Eureka"),
+    collapse.df(shp@data[arc.dense.rows,],"Arcata"),
+    collapse.df(shp@data[for.dense.rows,],"Fortuna"),
+    collapse.df(shp@data[mck.dense.rows,],"McKinleyville"))
+  placemark.header <- "	<Folder>
+		<name>Charger Distribution</name>
+		<open>1</open>"
+  placemark.styles <- ddply(rbind(shp@data,shp.sparse),.(row),function(df){ 
+    data.frame(style=paste("<Style id=\"placemark_",df$name,"\">
+		<IconStyle>",
+		if(roundC(1500*as.numeric(df$frac.homes),0)<1) "<color>00ffffff</color>
+		<scale>1.75</scale>" 
+		else 
+		"<scale>1.75</scale>","
+			<Icon>
+				<href>http://schatzlab.org/images/pev-charger-icons/ev-icon-",roundC(1500*as.numeric(df$frac.homes),0),".png</href>
+			</Icon>
+			<hotSpot x=\"0.5\" y=\"0\" xunits=\"fraction\" yunits=\"fraction\"/>
+		</IconStyle>",
+		if(roundC(1500*as.numeric(df$frac.homes),0)==0) "<LabelStyle>
+			<color>00ffffff</color>
+			</LabelStyle>","
+  </Style>",sep=''),stringsAsFactors=F)
+  })$style
+  placemark.kml <- c("<Folder><name>Regular Zones</name><open>1</open>
+			<Region>
+				<LatLonAltBox>
+					<north>41.5</north>
+					<south>40</south>
+					<east>-123.4</east>
+					<west>-124.4</west>
+				</LatLonAltBox>
+				<Lod>
+					<minLodPixels>256</minLodPixels>
+				</Lod>	
+			</Region>",ddply(shp@data[-dense.rows,],.(row),function(df){ 
+    data.frame(placemark=paste("
+			<Placemark>
+				<name>",df$name,"</name>
+				<styleUrl>#placemark_",df$name,"</styleUrl>
+				<Point>
+					<coordinates>",
+					ifelse(any(df$name==hard.code.coords$name),paste(hard.code.coords[hard.code.coords$name==df$name,"lon"],",",hard.code.coords[hard.code.coords$name==df$name,"lat"],",",0),paste(df$long,",",df$lat,",0")),"</coordinates>
+				</Point>
+			</Placemark>",sep=''),stringsAsFactors=F)
+  })$placemark,"</Folder>")
+  placemark.kml <- c(placemark.kml,"<Folder>
+		<name>Aggregated Zones</name>
+		<open>1</open>
+        ",ddply(shp.sparse,.(row),function(df){ 
+    data.frame(placemark=paste("
+			<Placemark>
+        <Region>
+          <LatLonAltBox>
+            <north>",df$lat+0.75,"</north>
+            <south>",df$lat-0.75,"</south>
+            <east>",df$long+0.5,"</east>
+            <west>",df$long-0.5,"</west>
+          </LatLonAltBox>
+          <Lod>
+            <minLodPixels>256</minLodPixels>
+            <maxLodPixels>8192</maxLodPixels>
+          </Lod>	
+        </Region>
+				<name>",df$name,"</name>
+				<styleUrl>#placemark_",df$name,"</styleUrl>
+				<Point>
+					<coordinates>",
+					ifelse(any(df$name==hard.code.coords$name),paste(hard.code.coords[hard.code.coords$name==df$name,"lon"],",",hard.code.coords[hard.code.coords$name==df$name,"lat"],",",0),paste(df$long,",",df$lat,",0")),"</coordinates>
+				</Point>
+			</Placemark>",sep=''),stringsAsFactors=F)
+  })$placemark,"</Folder>")
+  dense.placemark.kml <- ""
+  for(sparse.name in c("Arcata","Eureka","McKinleyville","Fortuna")){
+    shp.row <- which(shp.sparse$name == sparse.name)
+    dense.placemark.kml <- c(dense.placemark.kml,paste("					<Folder>
+              <name>",sparse.name," Chargers</name>
+              <open>1</open>
+              <Region>
+                <LatLonAltBox>
+                  <north>",shp.sparse$lat[shp.row]+0.04,"</north>
+                  <south>",shp.sparse$lat[shp.row]-0.04,"</south>
+                  <east>",shp.sparse$long[shp.row]+0.02,"</east>
+                  <west>",shp.sparse$long[shp.row]-0.02,"</west>
+                </LatLonAltBox>
+                <Lod>
+                  <minLodPixels>256</minLodPixels>
+                  <maxLodPixels>-1</maxLodPixels>
+                </Lod>
+              </Region>",sep=''))
+    use.rows <- unlist(ifelse(sparse.name=="Arcata",list(arc.dense.rows),
+                  ifelse(sparse.name=="Eureka",list(eka.dense.rows),
+                    ifelse(sparse.name=="McKinleyville",list(mck.dense.rows),list(for.dense.rows)))))
+    dense.placemark.kml <- c(dense.placemark.kml,ddply(shp@data[use.rows,],.(row),function(df){ 
+      data.frame(placemark=paste("
+        <Placemark>
+          <name>",df$name,"</name>
+          <styleUrl>#placemark_",df$name,"</styleUrl>
+          <Point>
+						<coordinates>",
+						ifelse(any(df$name==hard.code.coords$name),paste(hard.code.coords[hard.code.coords$name==df$name,"lon"],",",hard.code.coords[hard.code.coords$name==df$name,"lat"],",",0),paste(df$long,",",df$lat,",0")),"</coordinates>
+					</Point>
+        </Placemark>",sep=''),stringsAsFactors=F)
+    })$placemark)
+    dense.placemark.kml <- c(dense.placemark.kml,"</Folder>")
+  }
+
+  placemark.footer <- "	</Folder>"
+  cat(placemark.header, file=kmlFile, sep="\n")
+  cat(placemark.styles, file=kmlFile, sep="\n")
+  cat(placemark.kml, file=kmlFile, sep="\n")
+  cat(dense.placemark.kml, file=kmlFile, sep="\n")
+  cat(placemark.footer, file=kmlFile, sep="\n")
+  cat(unlist(kml.data["style",]), file=kmlFile, sep="\n")
+  cat(unlist(kml.data["content",]), file=kmlFile, sep="\n")
+  cat(kmlPolygon()$footer, file=kmlFile, sep="\n")
+  close(kmlFile)
+
+  system(paste('open ',kml.filename,sep=''))
+}
 print('map.color, shp.to.kml loaded')

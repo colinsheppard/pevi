3b74c4c6bb89ced98e26aa0b07cd7107a4caa7e4
colinsheppard <colinthegaunt@gmail.com>
2013-02-12 12:46:02 -0800

    moved hard coded coords to csv file to make easier to modify

--- R/hard-coded-coords.R
+++
@@ -1,4 +0,0 @@
-name <- c("EKA_HendersonCenter","Loleta","WestEnd","FOR_North")
-lat <- c(40.768460,40.664089,40.885970,40.600869)
-lon <- c(-124.158576,-124.257822,-124.031425,-124.136831)
-hard.code.coords <- data.frame(name=name,lat=lat,lon=lon,stringsAsFactors=FALSE)
--- R/optim/analyze-optim.R
+++ R/optim/analyze-optim.R
@@ -8,6 +8,7 @@ path.to.pevi <- paste(base.path,'pevi/',sep='')
 path.to.inputs <- paste(base.path,'pev-shared/data/inputs/optim/',sep='')
 path.to.outputs <- paste(base.path,'pev-shared/data/outputs/optim/',sep='')
 path.to.google <- paste(base.path,'pev-shared/data/google-earth/',sep='')
+path.to.geatm <- paste(base.path,'pev-shared/data/GEATM-2020/',sep='')
 nl.path <- "/Applications/NetLogo\ 5.0.3"
 model.path <- paste(path.to.pevi,"netlogo/PEVI-nolog.nlogo",sep='')
 
@@ -17,9 +18,9 @@ model.path <- paste(path.to.pevi,"netlogo/PEVI-nolog.nlogo",sep='')
 #optim.code <- 'min-cost-constrained-by-num-stranded'
 optim.code <- 'min-frac-stranded-constrained-by-cost-50-50'
 
-source(paste(path.to.pevi,"R/hard-coded-coords.R",sep=''))
+hard.code.coords <- read.csv(paste(path.to.google,"hard-coded-coords.csv",sep=''))
 source(paste(path.to.pevi,'R/gis-functions.R',sep=''))
-source(paste(path.to.pevi,"R/optim/optim-functions.R",sep='')) # note this will in turn call optim-config, objectives, and constraints
+source(paste(path.to.pevi,"R/optim/optim-functions.R",sep='')) 
 source(paste(path.to.pevi,"R/optim/optim-config.R",sep=''))
 source(paste(path.to.pevi,"R/optim/objectives.R",sep=''))
 source(paste(path.to.pevi,"R/optim/constraints.R",sep=''))
@@ -30,11 +31,19 @@ agg.taz <- readShapePoly(paste(path.to.google,'aggregated-taz-with-weights/aggre
 load(paste(path.to.google,'aggregated-taz-with-weights/aggregated-taz-with-weights-fieldnames.Rdata',sep=''))
 names(agg.taz@data) <- c("SP_ID",taz.shp.fieldnames)
 
+# load dist times
+dist <- read.csv(file=paste(path.to.geatm,'taz-dist-time.csv',sep=''))
+names(dist)[1] <- "from"
+
+# save paths needed in this loop
+optim.code.save <- optim.code
+base.path.save <- base.path
+
 for(pev.penetration in c(0.005,0.01,0.02,0.04)){
   #pev.penetration <- 0.02
   load(paste(path.to.outputs,optim.code,"/0saved-state-pen",pev.penetration*100,".Rdata",sep=''))
-  optim.code <- 'min-frac-stranded-constrained-by-cost-50-50'
-  path.to.outputs <- paste(base.path,'pev-shared/data/outputs/optim/',sep='')
+  optim.code <- optim.code.save
+  path.to.outputs <- paste(base.path.save,'pev-shared/data/outputs/optim/',sep='')
 
   final.gen <- gen.num - 1
   ptx.m <- melt(data.frame(ptx=1:(nrow(all.ptx[,,1])),all.ptx[,1:(ncol(all.ptx[,,1])-1),final.gen]),id.vars=c("ptx"))
@@ -55,11 +64,11 @@ for(pev.penetration in c(0.005,0.01,0.02,0.04)){
   agg.taz@data$L2 <- tot.by.taz$L2[match(agg.taz$id,tot.by.taz$taz)]
   agg.taz@data$L3 <- tot.by.taz$L3[match(agg.taz$id,tot.by.taz$taz)]
   agg.taz@data$charger.score <- tot.by.taz$charger.score[match(agg.taz$id,tot.by.taz$taz)]
-  #agg.taz@data$weighted.demand <- roundC(agg.taz@data$weighted.demand,0)
-  #agg.taz@data$frac.homes <- roundC(agg.taz@data$frac.homes,3)
+  agg.taz@data$weighted.demand <- roundC(agg.taz@data$weighted.demand,0)
+  agg.taz@data$frac.homes <- roundC(agg.taz@data$frac.homes,3)
   c.map <- paste(map.color(agg.taz@data$charger.score,blue2red(50)),'7F',sep='')
   chargers.to.kml(agg.taz,paste(path.to.google,'optim/',optim.code,'-pen',100*pev.penetration,'.kml',sep=''),paste('Chargers: Pen ',100*pev.penetration,'% Optimization: ',optim.code,sep=''),'Color denotes total chargers in each TAZ with L3 counting for 2 chargers (click to get actual # chargers).','red',1.5,c.map,id.col='ID',name.col='name',description.cols=c('id','name','L2','L3','weighted.demand','frac.homes'))
-  demand.to.kml(agg.taz,paste(path.to.google,'optim/',optim.code,'-pen',100*pev.penetration,'demand.kml',sep=''),paste('Demand: Pen ',100*pev.penetration,'% Optimization: ',optim.code,sep=''),'Color denotes total chargers in each TAZ with L3 counting for 2 chargers (click to get actual # chargers). Icon displays the number of electric vehicles native to the TAZ. Icon image courtesy of Danilo Rizzuti / FreeDigitalPhotos.net','red',1.5,c.map,id.col='ID',name.col='name',description.cols=c('id','name','L2','L3','weighted.demand','frac.homes'))
+  #demand.to.kml(agg.taz,paste(path.to.google,'optim/',optim.code,'-pen',100*pev.penetration,'demand.kml',sep=''),paste('Demand: Pen ',100*pev.penetration,'% Optimization: ',optim.code,sep=''),'Color denotes total chargers in each TAZ with L3 counting for 2 chargers (click to get actual # chargers). Icon displays the number of electric vehicles native to the TAZ. Icon image courtesy of Danilo Rizzuti / FreeDigitalPhotos.net','red',1.5,c.map,id.col='ID',name.col='name',description.cols=c('id','name','L2','L3','weighted.demand','frac.homes'))
   to.csv <- agg.taz@data[,c('id','name','L2','L3')]
   to.csv$cost <- 8 * to.csv$L2 + 50 * to.csv$L3
   to.csv$power <- 6.6 * to.csv$L2 + 30 * to.csv$L3
@@ -67,8 +76,8 @@ for(pev.penetration in c(0.005,0.01,0.02,0.04)){
   agg.taz@data$L2 <- all.ptx[which.min(all.ptx[,'fitness',final.gen]),(1:52)[agg.taz$id],final.gen]
   agg.taz@data$L3 <- all.ptx[which.min(all.ptx[,'fitness',final.gen]),(53:104)[agg.taz$id],final.gen]
   to.csv <- agg.taz@data[,c('id','name','L2','L3')]
-  to.csv$cost <- 8 * to.csv$L2 + 50 * to.csv$L3
-  to.csv$power <- 6.6 * to.csv$L2 + 30 * to.csv$L3
+  #to.csv$cost <- 8 * to.csv$L2 + 50 * to.csv$L3
+  #to.csv$power <- 6.6 * to.csv$L2 + 30 * to.csv$L3
   # min distances between chargers
   l2.l3.tazs <- agg.taz$id[which(agg.taz@data$L2>0 | agg.taz@data$L3>0)]
   for(taz.id in agg.taz$id){ 

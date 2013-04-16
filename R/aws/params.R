6db4c329108576340e1e1a02327993462eee3c51
Colin Sheppard <colinthegaunt@gmail.com>
2013-04-16 16:58:41 -0700

    needed for hot starts

--- R/aws/params.R
+++ R/aws/params.R
@@ -1,8 +1,12 @@
 
 base.path <- paste(getwd(),'/',sep='')
 
-optim.scenario <- 'cost'
+#optim.scenario <- 'cost'    # vary L2 installed cost
+#optim.scenario <- 'battery' # vary batter capacity
+optim.scenario <- 'type'    # vary BEV/PHEV ratio
 
-num.cpu <- 2
+num.cpu <- 8
 
 path.to.outputs.base <- paste(base.path,'pevi/outputs/',sep='')
+
+hot.start <- T

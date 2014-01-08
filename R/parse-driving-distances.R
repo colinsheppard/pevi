1245dbc8a6201527e1f6386d0539951787cac1e3
colinsheppard <colinthegaunt@gmail.com>
2014-01-07 16:15:32 -0800

    major revision to get full data set with appropriate names loaded

--- R/parse-driving-distances.R
+++ R/parse-driving-distances.R
@@ -1,30 +1,33 @@
 
 path.to.upstate.data <- pp(pevi.shared,'data/UPSTATE/driving-distances/')
 
-origin <- vector()
-destination <- vector()
-time <- vector()
-distance <- vector()
+origin <- c()
+destination <- c()
+time <- c()
+orig.name <- c()
+distance <- c()
 
-
-for(i in 1:length(list.files(path.to.upstate.data))) {
-	parsed.xml <- xmlTreeParse(pp(path.to.upstate.data,list.files(path.to.upstate.data)[i]))
-	ref.origin <- xmlRoot(parsed.xml)[[2]][[1]]
+the.files <- grep('Rdata',list.files(path.to.upstate.data,'.xml'),value=T,invert=T)
+for(the.file in the.files){
+  taz.name <- strsplit(strsplit(the.file,'.xml')[[1]],'-2')[[1]]
+	parsed.xml <- xmlTreeParse(pp(path.to.upstate.data,the.file))
+	ref.origin <- unlist(xmlRoot(parsed.xml)[[2]][[1]])['value']
 	time.dist.xml <- xmlRoot(parsed.xml)[['row']]
 	for(j in 1:xmlSize(time.dist.xml)) {
-		origin <- rbind(origin,ref.origin)
-		destination <- rbind(destination,xmlRoot(parsed.xml)[[j+2]][[1]])
-		time <- rbind(time,time.dist.xml[[j]][[2]][[2]][[1]])
-		distance <- rbind(distance,time.dist.xml[[j]][[3]][[2]][[1]])
+		origin <- c(origin,ref.origin)
+		destination <- c(destination,unlist(xmlRoot(parsed.xml)[[j+2]][[1]])['value'])
+		time <- c(time,unlist(time.dist.xml[[j]][[2]][[2]][[1]])['value'])
+    orig.name <- c(orig.name,taz.name)
+		distance <- c(distance,unlist(time.dist.xml[[j]][[3]][[2]][[1]])['value'])
 	}
 }
 
-time.distance <- data.frame(unlist(origin),unlist(destination),unlist(time),unlist(distance))
+time.distance <- data.frame(origin,destination,time,distance,orig.name,stringsAsFactors=F)
 
 ###########################################################################
 #  Convert google rep of time and distance into doubles
 ###########################################################################
-taz.time.distance$hours <- ldply(str_split(as.character(taz.time.distance$time)," hour"),function(v){ 
+time.distance$hours <- ldply(str_split(as.character(time.distance$time)," hour"),function(v){ 
   if(length(v)>1){
     hour <- as.numeric(v[1])
     min.str <- v[2]
@@ -35,7 +38,7 @@ taz.time.distance$hours <- ldply(str_split(as.character(taz.time.distance$time),
   if(substr(min.str,1,1)=="s")min.str <- substr(min.str,3,nchar(min.str))
   data.frame(hours=hour + as.numeric(str_split(min.str," min")[[1]][1])/60)
 })$hours
-taz.time.distance$miles <- ldply(str_split(as.character(taz.time.distance$distance)," mi"),function(v){
+time.distance$miles <- ldply(str_split(as.character(time.distance$distance)," mi"),function(v){
   if(length(v)>1){
     dist <- as.numeric(v[1])
   }else{
@@ -45,87 +48,42 @@ taz.time.distance$miles <- ldply(str_split(as.character(taz.time.distance$distan
 })$dist
 
 ###########################################################################
-#  Rename TAZs to be consistent across other data inputs
+#  Get the naming to all be based on orig.name
 ###########################################################################
-to.replace <- data.frame(orig=c('Proberta','Platina Center','RED-ForestHills','COTTON_West','COTTON_East','SHALKE_BearMountain','SHALKE_Downtown','SHALKE_ShastaDam','RED_MunicipalAirport','RoundMountainMontgomeryCreekOakRun','FallRiverMcArthur','LakeheadLakeshoreOBrien','Mt. Shasta','BurneyHatCreekCassel'),
-  replacement=c('Gerber','Platina','RED_ForestHills','COT_West','COT_East','SHA_BearMountain','SHA_Downtown','SHA_ShastaDam','RED_Airport','RoundMountain','FallRiver','Lakehead','Shasta','Burney'),stringsAsFactors=F)
-for(i in 1:nrow(to.replace)){
-  taz.time.distance$destination.taz[taz.time.distance$destination.taz==to.replace$orig[i]] <- to.replace$replacement[i]
-  taz.time.distance$origin.taz[taz.time.distance$origin.taz==to.replace$orig[i]] <- to.replace$replacement[i]
-}
-save(taz.time.distance,file=pp(pevi.shared,'data/UPSTATE/driving-distances/taz_time_distance.Rdata'))
+time.distance$dest <- time.distance$orig.name[match(time.distance$destination,time.distance$origin)]
+# couple of unique cases
+time.distance$dest[time.distance$destination=="3030 Shasta View Drive, Redding, CA 96002, USA"] <- "RED_ForestHills"
+time.distance$dest[time.distance$destination=="22663 Flores Avenue, Red Bluff, CA 96080, USA"] <- "Gerber"
+time.distance$dest[time.distance$destination=="12113-12119 Railroad Avenue East, Macdoel, CA 96058, USA"] <- "Macdoel"
+time.distance$dest[time.distance$destination=="116 East First Street, Dorris, CA 96023, USA"] <- "Dorris"
+time.distance$dest[time.distance$destination=="356 Lawndale Court, McCloud, CA 96057, USA"] <- "McCloud"
+time.distance$dest[time.distance$destination=="Corning Road & Interstate 5, Corning, CA 96021, USA"] <- "Corning"
+time.distance$dest[time.distance$destination=="Browning Street, Redding, CA 96003, USA"] <- "RED_BoulderCreek"
+time.distance$dest[time.distance$destination=="112 Kingsview Court, Redding, CA 96003, USA"] <- "RED_LakeRedding"
+time.distance$dest[time.distance$destination=="2301 Duncan Lane, Redding, CA 96002, USA"] <- "RED_Pacheco"
+time.distance$dest[time.distance$destination=="1930 North Street, Anderson, CA 96007, USA"] <- "AND_Cascade"
+time.distance$dest[time.distance$destination=="4450-4498 California 151, Shasta Lake, CA 96019, USA"] <- "SHA_Downtown"
+time.distance$dest[time.distance$destination=="15915 Dragon Fly Drive, Oak Run, CA 96069, USA"] <- "RoundMountain"
+time.distance$dest[time.distance$destination=="5001 Bechelli Lane, Redding, CA 96002, USA"] <- "RED_LaytonOaks"
+time.distance$dest[time.distance$destination=="1898 Benton Drive, Redding, CA 96003, USA"] <- "RED_LakeRedding"
+time.distance$dest[time.distance$destination=="1915-1941 Rancho Road, Redding, CA 96002, USA"] <- "RED_ChurnCreek"
+time.distance$dest[time.distance$destination=="21114 Golden Trail West, Redding, CA 96003, USA"] <- "SHA_BearMountain"
+dest.to.exclude <- c('50976-51798 Klamath River Highway, Klamath National Forest, Seiad Valley, CA 96086, USA',
+                      '22542-23316 Klamath River Highway, Klamath National Forest, Klamath River, CA 96050, USA',
+                      '4043-5285 Central Avenue, Weed, CA 96094, USA',
+                      'Forest Route 46N50, Klamath National Forest, Klamath River, CA 96050, USA',
+                      '2401-2517 Klamath River Highway, Yreka, CA 96097, USA',
+                      'Seiad Oaks Road, Klamath National Forest, Seiad Valley, CA 96086, USA',
+                      'Mill Road, Klamath National Forest, Happy Camp, CA 96039, USA',
+                      '1200 Cherry Maple Road, Klamath National Forest, Horse Creek, CA 96050, USA',
+                      '4001-5285 Center Street, Weed, CA 96094, USA',
+                      '3001 California 273, Anderson, CA 96007, USA')
+time.distance <- subset(time.distance,!destination %in% dest.to.exclude)
+time.distance$orig <- time.distance$orig.name
+time.distance <- time.distance[,c('orig','dest','hours','miles')]
+time.distance <- rbind(time.distance,data.frame(orig='RED_MetzRoad',dest='AND_Cascade',hours=0.15,miles=4.6))
 
-# Andy's renaming stuff
+time.distance <- unique(data.table(time.distance,key=c('orig','dest')))
 
-update.time.distance <- data.frame(unlist(origin[,6]),unlist(destination[,6]),unlist(time[,6]),unlist(distance[,6]))
-
-update.time.distance$origin.taz <- ''
-update.time.distance$destination.taz <- ''
-
-for(i in 1:nrow(taz.time.distance)) {
-	if(update.time.distance$origin[i] %in% taz.time.distance$origin) {
-		update.time.distance$origin.taz[i] <- taz.time.distance$origin.taz[match(update.time.distance$origin[i],taz.time.distance$origin)]
-	}
-	if(update.time.distance$destination[i] %in% taz.time.distance$destination.address) {
-		update.time.distance$destination.taz[i] <- as.character(taz.time.distance$destination.taz[match(update.time.distance$destination[i],taz.time.distance$destination.address)])
-	}
-}
-
-for(i in 1:nrow(update.time.distance)) {
-# Fix the origin addresses first
-	if(update.time.distance$destination[i]=='2600-2998 Balls Ferry Road, Anderson, CA 96007, USA') {
-		 update.time.distance$destination.taz[i] <- 'AND_Cascade'
-	}
-	if(update.time.distance$destination[i]=='945-1001 Highway 99 W, Corning, CA 96021, USA') {
- 					 update.time.distance$destination.taz[i] <- 'Corning'
-	}
-	if(update.time.distance$destination[i]=='3571-3591 Park Drive, Cottonwood, CA 96022, USA') {
-			 update.time.distance$destination.taz[i] <- 'COTTON_West'
-	}
-	if(update.time.distance$destination[i]=='Railroad Avenue, Dorris, CA 96023, USA') {
-			 update.time.distance$destination.taz[i] <- 'Dorris'
-	}
-	if(update.time.distance$destination[i]=='327-337 Collier Way, Etna, CA 96027, USA') {
-			 update.time.distance$destination.taz[i] <- 'Etna'
-	}
-	if(update.time.distance$destination[i]=='11819 Main Street, Fort Jones, CA 96032, USA') {
-			 update.time.distance$destination.taz[i] <- 'Fort Jones'
-	}
-	if(update.time.distance$destination[i]=='7918 Railroad Avenue, Los Molinos, CA 96055, USA') {
-			 update.time.distance$destination.taz[i] <- 'Los Molinos'
-	}
-	if(update.time.distance$destination[i]=='11816-11826 Railroad Avenue East, Macdoel, CA 96058, USA') {
-			 update.time.distance$destination.taz[i] <- 'Macdoel'
-	}
-	if(update.time.distance$destination[i]=='480 California 89, McCloud, CA 96057, USA') {
-			 update.time.distance$destination.taz[i] <- 'McCloud'
-	}
-	if(update.time.distance$destination[i]=='4558-4650 California 36, Platina, CA 96076, USA') {
-			 update.time.distance$destination.taz[i] <- 'Platina'
-	}
-	if(update.time.distance$destination[i]=='1001-1199 Churn Creek Road, Redding, CA 96003, USA') {
-			 update.time.distance$destination.taz[i] <- 'RED_BoulderCreek'
-	}
-	if(update.time.distance$destination[i]=='4465 Airport Road, Redding, CA 96002, USA') {
-			 update.time.distance$destination.taz[i] <- 'RED_CloverCreek'
-	}
-	if(update.time.distance$destination[i]=='11537 Ridgewood Road, Redding, CA 96003, USA') {
-			 update.time.distance$destination.taz[i] <- 'RED_GoldHills'
-	}
-	if(update.time.distance$destination[i]=='7223 Churn Creek Road, Redding, CA 96002, USA') {
-			 update.time.distance$destination.taz[i] <- 'RED_Pacheco'
-	}
-	if(update.time.distance$destination[i]=='9874 Old Oregon Trail, Redding, California 96003, USA') {
-			 update.time.distance$destination.taz[i] <- 'RED_SportsComplex'
-	}
-	if(update.time.distance$destination[i]=='3001-3031 Shasta View Drive, Redding, CA 96002, USA') {
-			 update.time.distance$destination.taz[i] <- 'RED_ForestHills'
-	}
- 	if(update.time.distance$destination[i]=='344 Black Canyon Road, Shasta Lake, CA 96019, USA') {
- 			 update.time.distance$destination.taz[i] <- 'SHALKE_ShastaDam'
-	}
-	if(update.time.distance$destination[i]=='30659-30667 Whitmore Road, Whitmore, CA 96096, USA') {
-			 update.time.distance$destination.taz[i] <- 'Whitmore'
-	}	
-}
+save(time.distance,file=pp(pevi.shared,'data/UPSTATE/driving-distances/time.distance.Rdata'))
 

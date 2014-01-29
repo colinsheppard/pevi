293a629411aac63813249683cf777ae1c4771844
Andy Harris <andrew.harris@humboldt.edu>
2014-01-29 13:21:46 -0800

    Added script to update the upstate TAZs

---
+++ R/upstate/update-upstate-driver-schedules.R
@@ -0,0 +1,16 @@
+# Load the conversion table from old TAZ to new TAZ.
+load(pp(pevi.shared,'data/UPSTATE/od.converter.Rdata'))
+
+# Cycle through the uncombined files and convert to the new TAZ ids.
+for(od.input.file in list.files(pp(pevi.shared,'data/inputs/driver-input-file/upstate-uncombined/'))){
+	od.table <- read.table(pp(pevi.shared,'data/inputs/driver-input-file/upstate-uncombined/',od.input.file),sep='\t',header=TRUE)
+	names(od.table) <- c(';driver','from','to','depart','home')
+	for(i in 1:nrow(od.table)) {
+		od.table$from[i] <- od.converter$new.id[match(od.table$from[i],od.converter$old.id)]
+		od.table$to[i] <- od.converter$new.id[match(od.table$to[i],od.converter$old.id)]
+		od.table$home[i] <- od.converter$new.id[match(od.table$home[i],od.converter$old.id)]
+	}
+	write.table(od.table,file=pp(pevi.shared,'data/inputs/driver-input-file/upstate-uncombined/updated/',substr(od.input.file,1,nchar(od.input.file)-4),'.txt'),sep='\t',row.names=F)
+}
+
+# The frst line will need to be adjusted to remove the quotes for use in NetLogo.

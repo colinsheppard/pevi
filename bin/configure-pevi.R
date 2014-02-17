5a72dafa3373f7cb5872f666d82d4235232e1d3c
colinsheppard <colinthegaunt@gmail.com>
2014-02-17 10:12:34 -0800

    missing comma

--- bin/configure-pevi.R
+++ bin/configure-pevi.R
@@ -24,7 +24,7 @@ load.libraries(c('optparse','stringr'))
 option_list <- list(
   make_option(c("-p", "--pevi"), default="~/Dropbox/pevi", help="Path to PEVI repository [default %default]"),
   make_option(c("-s", "--shared"), default="~/Dropbox/pev-shared", help="Path to pev-shared directory [default %default]"),
-  make_option(c("-n", "--nondrop"), default="~/Documents/pevi", help="Path to non-dropbox directory for pevi [default %default]")
+  make_option(c("-n", "--nondrop"), default="~/Documents/pevi", help="Path to non-dropbox directory for pevi [default %default]"),
   make_option(c("-c", "--numcpu"), default="1", help="Number of CPUs you want this machine to use for parallel analysis [default %default]"),
 )
 if(interactive()){

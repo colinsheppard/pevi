fd565bde01d43de23f52543db6fb1eb9828e7028
Colin Sheppard <colinthegaunt@gmail.com>
2014-03-13 07:38:35 -0700

    made ddply more generic

--- R/upstate/analyze-optim.R
+++ R/upstate/analyze-optim.R
@@ -55,19 +55,6 @@ ch.fin <- ddply(subset(chs,TAZ>0),.(scenario,seed,penetration),function(df){
 })
 ch.fin.unshaped <- ch.fin
 
-# Now to calculate some average buildouts. First, let's average the final buildout between all seeds.
-# The resulting data frame should have for each row a unique penetration/taz/level combination, and the
-# remaining columns will be the total for each scenario.
-
-mean.ch.fin <- ddply(ch.fin,.(penetration,TAZ,level),function(df){
-	data.frame('new-obj' = round(mean(df$'new-obj',na.rm=TRUE)),
-						 'L2-10k' = round(mean(df$'L2-10k',na.rm=TRUE)),
-						 'L2-20k' = round(mean(df$'L2-20k',na.rm=TRUE)),
-						 'opp-cost-10' = round(mean(df$'opp-cost-10',na.rm=TRUE)),
-						 'opp-cost-20' = round(mean(df$'opp-cost-20',na.rm=TRUE))
-)})
-names(mean.ch.fin) <- c('penetration','TAZ','level','new-obj','L2-10k','L2-20k','opp-cost-10','opp-cost-20')
-
 winners <- ddply(opts,.(scenario,seed,penetration,iteration),function(df){
   df[which.min(df$obj),]
 })
@@ -147,13 +134,23 @@ build.result.history.mean$rep[build.result.history.mean$obj.norm.mean > quantile
 load(pp(pevi.shared,'data/UPSTATE/shapefiles/AggregatedTAZsWithPointTAZs.Rdata'))
 load(pp(pevi.shared,'data/UPSTATE/od-converter.Rdata'))
 agg.taz$new.id <- od.converter$new.id[match(agg.taz$agg.id,od.converter$old.id)]
+agg.coords <- coordinates(agg.taz)
+
+# Now to calculate some average buildouts. First, let's average the final buildout between all seeds.
+# The resulting data frame should have for each row a unique penetration/taz/level combination, and the
+# remaining columns will be the total for each scenario.
+
+ch.fin <- subset(melt(ch.fin.unshaped,measure.vars=pp('L',0:4),variable_name="level"),level%in%pp('L',2:3))
+mean.ch.fin <- ddply(ch.fin,.(penetration,TAZ,level,scenario),function(df){
+	data.frame(num.chargers = round(mean(df$value,na.rm=TRUE)))
+})
 
 #l2.scenario <- subset(ch.fin,penetration==0.02 & seed==4 & level=='L2',select=c('penetration','TAZ','seed','level','new-obj'))
-l2.scenario <- subset(mean.ch.fin,penetration==0.02 & level=='L2',select=c('penetration','TAZ','level','new-obj'))
-agg.taz$L2 <- l2.scenario$'new-obj'[match(agg.taz$new.id,l2.scenario$TAZ)]
+l2.scenario <- subset(mean.ch.fin,penetration==0.02 & level=='L2' & scenario=='new-obj',select=c('TAZ','num.chargers'))
+agg.taz$L2 <- l2.scenario$num.chargers[match(agg.taz$new.id,l2.scenario$TAZ)]
 #l3.scenario <- subset(ch.fin,penetration==0.02 & seed==4 & level=='L3',select=c('penetration','TAZ','seed','level','new-obj'))
-l3.scenario <- subset(mean.ch.fin,penetration==0.02 & level=='L3',select=c('penetration','TAZ','level','new-obj'))
-agg.taz$L3 <- l3.scenario$'new-obj'[match(agg.taz$new.id,l3.scenario$TAZ)]
+l3.scenario <- subset(mean.ch.fin,penetration==0.02 & level=='L3' & scenario=='new-obj',select=c('TAZ','num.chargers'))
+agg.taz$L3 <- l3.scenario$num.chargers[match(agg.taz$new.id,l3.scenario$TAZ)]
 
 # For the charger files that wll get translated nto Google maps, we use mean.ch.fin
 # Each charger is defined by an array: 

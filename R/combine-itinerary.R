fe49a1e9722286e4e316e3797e0389e4725cbdd2
Colin Sheppard <colinthegaunt@gmail.com>
2014-04-15 16:33:51 -0700

    fixing bugs and adding quadrupled

--- R/combine-itinerary.R
+++ R/combine-itinerary.R
@@ -12,7 +12,6 @@ path.to.combined.half.homeless.inputs <- pp(pevi.shared,'data/inputs/driver-inpu
 pens <- c(0.005,0.01,0.02)
 schedule.reps[['0.01']] <- list()
 
-#path.to.combined <- path.to.combined.inputs
 for (pen in pens){
   prev.pen <- ifelse(pen==0.01,'0.005','0.01')
   #pen <- pens[1]
@@ -23,6 +22,7 @@ for (pen in pens){
     rep.2 <- 2*final.replicate
     if(pen==0.005){
       sched <- schedule.reps[['0.005']][[final.replicate]]
+      sched$depart <- sched$depart - 6
       next.sched <- data.frame()
     }else{
       sched <- schedule.reps[[prev.pen]][[rep.1]]
@@ -34,13 +34,31 @@ for (pen in pens){
       schedule.reps[['0.01']][[final.replicate]] <- new.sched
     }
     new.sched <- as.data.frame(new.sched[,list(from=rep(from,2),to=rep(to,2),depart=c(depart,depart+24),home=rep(home,2)),by='driver'])
-    for(path.to.combined in c(path.to.combined.inputs,path.to.combined.half.homeless.inputs,path.to.combined.homeless.inputs)){
-      if(path.to.combined == path.to.combined.half.homeless.inputs){
+    # fix inconsistency in drivers who don't end up the first day where they start
+    new.sched$day <- ifelse(new.sched$depart<30,1,2)
+    new.sched <- ddply(new.sched,.(driver),function(df){
+      df$from[which(df$day==2)[1]] <- tail(subset(df,day==1),1)$to
+      df
+    })
+    quad.sched <- as.data.frame(data.table(new.sched,key='driver')[,list(from=rep(from,2),to=rep(to,2),depart=c(depart,depart+48),home=rep(home,2)),by='driver'])
+    quad.sched$day <- ifelse(quad.sched$depart<30,1,ifelse(quad.sched$depart<54,2,ifelse(quad.sched$depart<78,3,4)))
+    quad.sched <- ddply(quad.sched,.(driver),function(df){
+      df$from[which(df$day==2)[1]] <- tail(subset(df,day==1),1)$to
+      df$from[which(df$day==3)[1]] <- tail(subset(df,day==2),1)$to
+      df$from[which(df$day==4)[1]] <- tail(subset(df,day==3),1)$to
+      df
+    })
+    new.sched <- new.sched[,1:(ncol(new.sched)-1)]
+    quad.sched <- quad.sched[,1:(ncol(quad.sched)-1)]
+    for(path.to.combined in pp(c(path.to.combined.inputs,path.to.combined.half.homeless.inputs,path.to.combined.homeless.inputs),'consistent/')){
+      #path.to.combined <- path.to.combined.inputs
+      make.dir(path.to.combined)
+      if(path.to.combined == pp(path.to.combined.half.homeless.inputs,'consistent/')){
         names(new.sched) <- c('driver',tail(names(new.sched),-1))
         evics <- data.frame(driver=unique(new.sched$driver),evict=sample(c(T,F),length(unique(new.sched$driver)),replace=T))
         evics.sched <- evics$evict[match(new.sched$driver,evics$driver)]
         new.sched$home[evics.sched] <- 0
-      }else if(path.to.combined == path.to.combined.homeless.inputs){
+      }else if(path.to.combined == pp(path.to.combined.homeless.inputs,'consistent/')){
         new.sched$home <- 0
       }
       names(new.sched) <- c(';driver',tail(names(new.sched),-1))
@@ -49,5 +67,30 @@ for (pen in pens){
       print(h(new.sched))
       print(sum(new.sched$home==0))
     }
+    for(path.to.combined in pp(c(path.to.combined.inputs,path.to.combined.half.homeless.inputs,path.to.combined.homeless.inputs),'quadrupled/')){
+      #path.to.combined <- path.to.combined.inputs
+      make.dir(path.to.combined)
+      if(path.to.combined == pp(path.to.combined.half.homeless.inputs,'quadrupled/')){
+        names(quad.sched) <- c('driver',tail(names(quad.sched),-1))
+        evics <- data.frame(driver=unique(quad.sched$driver),evict=sample(c(T,F),length(unique(quad.sched$driver)),replace=T))
+        evics.sched <- evics$evict[match(quad.sched$driver,evics$driver)]
+        quad.sched$home[evics.sched] <- 0
+      }else if(path.to.combined == pp(path.to.combined.homeless.inputs,'quadrupled/')){
+        quad.sched$home <- 0
+      }
+      names(quad.sched) <- c(';driver',tail(names(quad.sched),-1))
+      write.table(quad.sched[,c(';driver','from','to','depart','home')],file=pp(path.to.combined,"driver-schedule-pen",pen*100,"-rep",final.replicate,"-20140217.txt"),sep='\t',row.names=F,quote=F)
+      print(pp(path.to.combined,"driver-schedule-pen",pen*100,"-rep",final.replicate,"-20140217.txt"))
+      print(h(quad.sched))
+      print(sum(quad.sched$home==0))
+    }
   }
 }
+
+# analyze incidence of teleportation
+# result: 0.67% of drivers teleport
+cons <- ddply(new.sched,.(driver),function(df){
+  end.pt <- tail(subset(df,day==1),1)$to
+  st.pt <- head(subset(df,day==1),2)$from
+  data.frame(consistent=end.pt==st.pt)
+})

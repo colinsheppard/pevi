540c45a0cb4e759721322f65eba9dc9d2bdc7865
Colin Sheppard <colinthegaunt@gmail.com>
2015-09-03 13:16:24 -0700

    batch optim scripts for running pevi

---
+++ bin/batch-optims-oct.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+optimize-pevi.R -d /Users/critter/Dropbox/serc/pev-colin/pev-shared/data/inputs/optim-new/delhi-swap/ -s 54 -v 2.1
+optimize-pevi.R -d /Users/critter/Dropbox/serc/pev-colin/pev-shared/data/inputs/optim-new/delhi-cheap-l4/ -s 54 -v 2.1
+optimize-pevi.R -d /Users/critter/Dropbox/serc/pev-colin/pev-shared/data/inputs/optim-new/delhi-cheap-l3/ -s 54 -v 2.1
---
+++ bin/batch-optims-serc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+optimize-pevi.R -d /Users/sheppardc/Dropbox/serc/pev-colin/pev-shared/data/inputs/optim-new/delhi-cheap-l1/ -s 53 -v 2.1
+optimize-pevi.R -d /Users/sheppardc/Dropbox/serc/pev-colin/pev-shared/data/inputs/optim-new/delhi-cheap-l2/ -s 53 -v 2.1
+optimize-pevi.R -d /Users/sheppardc/Dropbox/serc/pev-colin/pev-shared/data/inputs/optim-new/delhi-cheap-l3/ -s 53 -v 2.1
